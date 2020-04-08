b75111c0d21887356708a4aa35a35f2d3251bd4f
Genuine <MasalitinJ@gmail.com>
2020-04-08 12:38:29 +0300

    Initial Commit

---
+++ RickAndMortyTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMortyTest
+//
+//  Created by Genuine on 08.04.2020.
+//  Copyright © 2020 Genuine. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
